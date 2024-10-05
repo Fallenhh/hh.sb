4c498352ff723b813c068cf456fc537efa6bb401
Yifan Hua <fallen2hh@gmail.com>
2024-10-04 17:25:01 -0700

    header z index

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from "waku";
 
 export const Header = () => {
   return (
-    <header className="flex flex-row columns-4 items-center lg:fixed md:fixed gap-4 p-6 left-0 top-0 w-screen bg-white drop-shadow h-16">
+    <header className="flex flex-row z-20 columns-4 items-center lg:fixed md:fixed gap-4 p-6 left-0 top-0 w-screen bg-white drop-shadow h-16">
       <div className="flex">
         <h2 className="header-item">
           <Link to="/">Home</Link>
